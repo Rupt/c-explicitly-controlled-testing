3c57b7cb1a123a677fe0a39489574dadfbd05f2a
Rupert Tombs <rupert.tombs@gmail.com>
2023-10-03 16:58:24 +0100

    simplify the loop example

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ static int test_demo() {
 }
 
 static int test_code() {
-  const char code[] = "--<-<<+[+[<+>--->->->-<<<]>]<<]--.";
+  const char code[] = "--<-<<+[+[<+>--->->->-<<<]>]<<--.<++++++.<<-..<<.<+.>>.";
 
   if (!test_once(sizeof(code) < 78)) // ✅
     return test_report("sizeof(code) == %zu", sizeof(code));
@@ -18,22 +18,9 @@ static int test_code() {
   if (!test_once(code[0] == '+')) // ❌
     return test_report("code[0] == '%c'", code[0]);
 
-  // After reporting any positive or negative result, we continue...
-  int n_open_brackets = 0;
+  // After reporting any positive or negative result, our loop continues.
   for (int i = 0; i < (int)sizeof(code); i++) {
-    switch (code[i]) {
-    case '[':
-      ++n_open_brackets;
-      break;
-    case ']':
-      --n_open_brackets;
-      break;
-    }
-    if (n_open_brackets > 0)
-      continue;
-
-    // ...to report on further checks. We must only close matched brackets.
-    if (!test_once(code[i] != ']')) // ❌
+    if (!test_once(code[i] != '.')) // ❌
       return test_report("code[%d] == '%c'", i, code[i]);
   }
 
